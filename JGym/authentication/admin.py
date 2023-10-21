5835512849fb270f76c6fcd67633ae801ac1c2e0
Rajandeep <rajandeepkaur61@outlook.com>
2023-10-21 12:34:27 -0700

    enrollment started

--- JGym/authentication/admin.py
+++ JGym/authentication/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from authentication.models import Contact
+from authentication.models import Contact,MembershipPlan,Trainer,Enrollment
 
 
 
 # Register your models here.
 admin.site.register(Contact)
+admin.site.register(MembershipPlan)
+admin.site.register(Trainer)
+admin.site.register(Enrollment)
+
 
